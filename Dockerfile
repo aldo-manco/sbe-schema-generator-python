0f63706a1fb6140246d17d68327039ad06ac0fba
aldo-manco-jr <aldo.manco@protonmail.ch>
2024-03-20 18:43:12 +0100

    docker_configured_correctly_improved_tables_identification_algorithm

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+# Usa come base l'immagine ufficiale di Ubuntu
+FROM pytorch/pytorch:1.10.0-cuda11.3-cudnn8-runtime
+
+# Imposta variabili di ambiente per evitare domande durante l'installazione
+ARG DEBIAN_FRONTEND=noninteractive
+
+# Aggiorna i pacchetti di sistema e installa dipendenze Python e altre utility necessarie
+RUN apt-get update && apt-get install -y \
+    python3-pip \
+    python3-dev \
+    git \
+    libgl1-mesa-glx \
+    tesseract-ocr \
+    libtesseract-dev \
+    && rm -rf /var/lib/apt/lists/*
+
+# Aggiorna pip e installa pacchetti Python richiesti
+RUN pip3 install --upgrade pip && \
+    pip3 install \
+    Pillow \
+    layoutparser[ocr] \
+    opencv-python
+
+RUN pip install detectron2 -f \
+  https://dl.fbaipublicfiles.com/detectron2/wheels/cu113/torch1.10/index.html
+
+# Imposta il workspace
+WORKDIR /container
+
+# Copia il codice sorgente nel container, assicurati di avere il codice sorgente nella stessa cartella di questo Dockerfile
+#COPY . /container
+
+# Comando di default per eseguire lo script python (adattalo al nome del tuo script)
+CMD ["python3", "app.py"]
--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 import cv2
 import layoutparser as lp
+import numpy as np
 
 def main():
 
@@ -15,7 +16,65 @@ def main():
     )
 
     layout = model.detect(image)
-    lp.draw_box(image, layout,)
+
+    lp.draw_box(image, layout, box_width=3)
+
+    tables = []
+    figures = []
+
+    for element in layout:
+        if element.type == "Table":
+            tables.append(element)
+        if element.type == "Figure":
+            figures.append(element)
+
+    layout_tables = lp.Layout(tables)
+    layout_figures = lp.Layout(figures)
+
+    not_overlapped_tables = []
+
+    for table in layout_tables:
+        is_overlapping = False
+
+        for figure in layout_figures:
+            if table.is_in(figure):
+                is_overlapping = True
+                break
+
+        if not is_overlapping:
+            not_overlapped_tables.append(table)
+
+    not_overlapped_tables = lp.Layout(not_overlapped_tables)
+
+    # height, width = image.shape[:2]
+    # left_interval = lp.Interval(0, width / 2 * 1.05, axis='x').put_on_canvas(image)
+    # left_blocks = text_blocks.filter_by(left_interval, center=True)
+    # left_blocks.sort(key=lambda element: element.coordinates[1])
+    #
+    # right_blocks = [element for element in text_blocks if element not in left_blocks]
+    # right_blocks.sort(key=lambda element: element.coordinates[1])
+    #
+    # text_blocks = lp.Layout([element.set(id=idx) for idx, element in enumerate(left_blocks + right_blocks)])
+
+    image_with_boxes = lp.draw_box(image, not_overlapped_tables, box_width=3, show_element_id=True)
+    image_with_boxes_np = np.array(image_with_boxes)
+    image_with_boxes_np = cv2.cvtColor(image_with_boxes_np, cv2.COLOR_RGB2BGR)
+    cv2.imwrite('processed_image.jpg', image_with_boxes_np)
+
+    ocr_agent = lp.TesseractAgent(languages='eng')
+
+    for table in not_overlapped_tables:
+        segment_image = (
+            table
+            # .pad(left=5, right=5, top=5, bottom=5)
+            .crop_image(image)
+        )
+
+        text = ocr_agent.detect(segment_image)
+        table.set(text=text, inplace=True)
+
+    for text in not_overlapped_tables.get_texts():
+        print(text, end='\n---\n')
 
 
 if __name__=="__main__":
